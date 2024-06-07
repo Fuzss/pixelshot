209cab09dbc54359d087c405c44cd37435dc6133
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-06-07 15:47:14 +0200

    overlay work

--- Common/src/main/java/fuzs/pixelshot/client/PixelshotClient.java
+++ Common/src/main/java/fuzs/pixelshot/client/PixelshotClient.java
@@ -5,9 +5,7 @@ import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
 import fuzs.puzzleslib.api.client.core.v1.context.KeyMappingsContext;
 import fuzs.puzzleslib.api.client.event.v1.ClientTickEvents;
 import fuzs.puzzleslib.api.client.event.v1.entity.player.ClientPlayerNetworkEvents;
-import fuzs.puzzleslib.api.client.event.v1.renderer.ComputeCameraAnglesCallback;
-import fuzs.puzzleslib.api.client.event.v1.renderer.FogEvents;
-import fuzs.puzzleslib.api.client.event.v1.renderer.GameRenderEvents;
+import fuzs.puzzleslib.api.client.event.v1.renderer.*;
 
 public class PixelshotClient implements ClientModConstructor {
 
@@ -17,20 +15,14 @@ public class PixelshotClient implements ClientModConstructor {
     }
 
     private static void registerEventHandlers() {
-//        ClientTickEvents.START.register(minecraft -> Mineshot.getOrthoViewHandler().onClientTickEvent());
-//        InputEvents.AFTER_KEY_ACTION.register(Mineshot.getOrthoViewHandler()::onKeyEvent);
-//        InputEvents.AFTER_KEY_ACTION.register(Mineshot.getScreenshotHandler()::onKeyEvent);
-//        GameRenderEvents.BEFORE.register(Mineshot.getScreenshotHandler()::onBeforeGameRender);
-//        ComputeCameraAnglesCallback.EVENT.register(Mineshot.getOrthoViewHandler()::onComputeCameraAngles);
-//        FogEvents.RENDER.register(Mineshot.getOrthoViewHandler()::onRenderFog);
-
-        // new
         ClientTickEvents.START.register(OrthoViewHandlerV2.getInstance()::onStartClientTick);
         GameRenderEvents.BEFORE.register(OrthoViewHandlerV2.getInstance()::onBeforeGameRender);
         GameRenderEvents.AFTER.register(OrthoViewHandlerV2.getInstance()::onAfterGameRender);
         ComputeCameraAnglesCallback.EVENT.register(OrthoViewHandlerV2.getInstance()::onComputeCameraAngles);
         FogEvents.RENDER.register(OrthoViewHandlerV2.getInstance()::onRenderFog);
         ClientPlayerNetworkEvents.LOGGED_IN.register(OrthoViewHandlerV2.getInstance()::onLoggedIn);
+        RenderGuiCallback.EVENT.register(OrthoViewHandlerV2.getInstance()::onRenderGui);
+        ComputeFieldOfViewCallback.EVENT.register(OrthoViewHandlerV2.getInstance()::onComputeFieldOfView);
     }
 
     @Override
