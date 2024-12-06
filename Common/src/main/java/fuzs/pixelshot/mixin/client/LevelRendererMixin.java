1e96507203441f184efa5aded731e37dc84a01e6
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-12-06 11:57:02 +0100

    prepare 1.21.3 port

---
+++ Common/src/main/java/fuzs/pixelshot/mixin/client/LevelRendererMixin.java
@@ -0,0 +1,19 @@
+package fuzs.pixelshot.mixin.client;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import fuzs.pixelshot.client.handler.OrthoViewHandler;
+import net.minecraft.client.renderer.LevelRenderer;
+import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LevelRenderer.class)
+abstract class LevelRendererMixin {
+
+    @Inject(method = "renderClouds", at = @At("HEAD"), cancellable = true)
+    public void renderClouds(PoseStack poseStack, Matrix4f frustumMatrix, Matrix4f projectionMatrix, float partialTick, double camX, double camY, double camZ, CallbackInfo callback) {
+        if (OrthoViewHandler.INSTANCE.isActive()) callback.cancel();
+    }
+}
